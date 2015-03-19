1bef8181eed8f8a9d34a15b822d23748915a8550
Chi <chijoungso@gmail.com>
2015-03-19 14:24:40 +0100

    Committen als een m***f***r

--- test/DAO/DAO_AdresTest.java
+++ test/DAO/DAO_AdresTest.java
@@ -12,8 +12,28 @@ import org.junit.Test;
 
 import domein_klassen.Adres;
 
-public class TestDAO_Adres {
-
+public class DAO_AdresTest {
+	public class DAO_Adres {
+	    static DAO_Manager manager;
+	    public DAO_AdresTest() {
+	    }
+	    
+	    @BeforeClass
+	    public static void setUpClass() {
+	        manager = new DAO_Manager();
+	        
+	    }
+	    
+	    @AfterClass
+	    public static void tearDownClass() {
+	        try{
+	            manager.closeConnection();
+	        }
+	        catch(SQLException ex){
+	            System.out.println("Connection is not closed");
+	        }
+	    }
+	    
 	@Test
 	public void testCreate() throws SQLException {
 		Adres adres = new Adres();
@@ -23,13 +43,13 @@ public class TestDAO_Adres {
 		adres.setToevoeging("b");
 		adres.setPostcode("2353AC");
 		adres.setWoonplaats("Utreg");
-				
-		DAO_Manager manager = new DAO_Manager();
+		
 		manager.getDAO_Adres().create(adres);
 		
 		int adresId = manager.getDAO_Adres().getAdresId(adres.getPostcode(), adres.getHuisnummer(), adres.getToevoeging());
 		System.out.println(adresId);
 		Adres adres2 = (Adres)manager.getDAO_Adres().read(adresId);
+		assertNotNull("Address from database not null?", adres2);
 		System.out.println(adres2.getStraatnaam());
 		assertEquals(adres, adres2);
 		
